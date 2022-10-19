a3d3a5d0e4ce71204c68583c9757352ab1a046d2
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-10-19 12:54:00 -0700

    first commit for NES Retro Gaming

---
+++ public/main.js
@@ -0,0 +1,7 @@
+let nes = document.querySelector('img');
+
+const helloAlert = evt => {
+    alert('Welcome to NES Retro Gaming!');
+}
+
+nes.addEventListener('click', helloAlert);
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+// app.use(express.static('public'));
+
+const { getHTML, getCSS, getJS } = require('./controller');
+
+app.get('/', getHTML);
+app.get('/css', getCSS);
+app.get('/js', getJS);
+
+const port = 4000//process.env.PORT //we don't have an env file but Heroku does so this reads from Heroku
+
+app.listen(port, console.log(`Server running on ${port}`))
