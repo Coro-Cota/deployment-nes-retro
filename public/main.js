6a2760aa5a0607d78367fbfc41c3ba9445ded904
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-10-19 13:23:43 -0700

    moved image folder to public folder

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 let nes = document.querySelector('img');
 
 const helloAlert = evt => {
-    alert('Welcome to NES Retro Gaming!');
+    alert('You just purchased 200 copies of E.T. for Atari!');
 }
 
 nes.addEventListener('click', helloAlert);
