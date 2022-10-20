100e8c449ebf0d41825a1873798cdad90d85e03e
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-10-20 11:35:57 -0700

    grammer

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 let nes = document.querySelector('img');
 
 const helloAlert = evt => {
-    alert('You just purchased 200 copies of E.T. for Atari!');
+    alert(`You've just purchased 200 copies of E.T. for Atari!`);
 }
 
 nes.addEventListener('click', helloAlert);
